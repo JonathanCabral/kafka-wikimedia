559a838a4a0ce4190369a44088a72a6640f9ca09
Jonathan Pereira Cabral <dev.jonathancabral@gmail.com>
2022-07-16 18:45:58 -0300

    Add properties to a safe kafka producer

--- producer/src/main/java/br/com/kafka/producer/WikimediaChangesProducer.java
+++ producer/src/main/java/br/com/kafka/producer/WikimediaChangesProducer.java
@@ -18,18 +18,27 @@ public class WikimediaChangesProducer {
 
     public static void main(String[] args) throws InterruptedException {
         String topic = "wikimedia.recentchange";
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
 
         //Create producer properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        /**
+         * SINCE KAFKA 3.0, THIS PROPERTIES ARE SET BY DEFAULT, AKA SAFE KAFKA PRODUCER
+         * So it's not needed to be set
+         */
+//        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+//        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+//        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "2147483647"); //Integer.MAX_VALUE
+//        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "120000"); //2 minutes
+//        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
         EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
-        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create("https://stream.wikimedia.org/v2/stream/recentchange"));
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
         EventSource eventSource = builder.build();
 
         //start the producer in another thread
